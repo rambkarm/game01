cb8d41843607b2dfa7cdf33c8a6b295f6bb992a6
rambkarm <rafaildolinskiy@gmail.com>
2022-11-16 19:39:26 +0200

    add profile menu.

--- src/main/java/org/example/RunApp.java
+++ src/main/java/org/example/RunApp.java
@@ -2,11 +2,20 @@ package org.example;
 
 public class RunApp {
     private static final String MENU = """
-            1 -- Enter name
-            2 -- Enter age
-            3 -- Enter salary
-            9 -- Exit game""";
-    private ConsoleUserDialog ui;
+            1 -- profile menu
+            2 -- character menu
+            3 -- games
+            4 -- load
+            5 -- save
+            9 -- exit game""";
+
+    private static final String PROFILE_MENU = """
+            1 -- enter name
+            2 -- enter age
+            3 -- enter email
+            4 -- enter password
+            9 -- back to main menu""";
+    private final ConsoleUserDialog ui;
 
     public RunApp() {
         this.ui = new ConsoleUserDialog();
@@ -20,8 +29,20 @@ public class RunApp {
         int choice;
         do {
             ui.printMessage(MENU);
-            choice = ui.enterInt("Choose choice menu item: ");
+            choice = ui.enterInt("choose choice menu item: ");
             ui.printMessage("your choice is: " + choice);
+            if(choice == 1){
+                createProfile();
+            }
         } while (choice != 9);
+    }
+
+    private void createProfile() {
+            ui.printMessage("---profile menu---");
+            String name = ui.enterString("enter name: ");
+            int age = ui.enterInt("enter age:");
+            String email = ui.enterString("enter email:");
+            String password = ui.enterString("enter password:");
+            ui.printMessage("---profile was updated---");
     }
 }
